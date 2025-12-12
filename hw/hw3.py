d5b12472583bab03078ffe54180b3a1d8d33c11a
tilenur <tilenur@gmail.com>
2025-12-13 01:20:33 +0900

    done hw3

--- hw/hw3.py
+++ hw/hw3.py
@@ -1,1 +1,83 @@
-gesdfa
+#1 Инкапсуляция
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self._price = price
+        self.__discount = 0
+
+    def set_discount(self, percent):
+        if percent <= 50:
+            self.__discount = percent
+        else:
+            return "there is an error with set_discount"
+    def apply_extra_discount(self, secret_code):
+        if secret_code == "VIP123":
+            self.__discount = self.__discount + 5
+        else:
+            return "Неверный код"
+    def get_price(self):
+        final_price = self._price * (1 - self.__discount / 100)
+        return final_price
+
+p = Product("Iphone", 1000)
+
+p.set_discount(20)
+print("Цена со скидкой:", p.get_price())
+
+p.apply_extra_discount("VIP123")
+print("Цена после VIP:", p.get_price())
+
+p.apply_extra_discount("wrong")
+print("Цена итоговая:", p.get_price())
+
+#2 Абстракция
+
+from abc import ABC, abstractmethod
+
+class PaymentMethod(ABC):
+
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+    @abstractmethod
+    def refund(self, amount):
+        pass
+
+class CardPayment(PaymentMethod):
+    def pay(self, amount):
+        return f"Оплата картой: {amount}"
+    def refund(self, amount):
+        return  f"Card refund: {amount}"
+
+class CashPayment(PaymentMethod):
+    def pay(self, amount):
+        return f"Оплата наличными: {amount}"
+    def refund(self, amount):
+        return  f"Cash refund: {amount}"
+
+class CryptoPayment(PaymentMethod):
+    def pay(self, amount):
+        return {
+            "type": "crypto",
+            "amount": amount,
+            "currency": "USDT"
+        }
+    def refund(self, amount):
+        return  f"Crypto refund: {amount}"
+
+class PaymentProcessor:
+    def __init__(self, payment_method):
+        self.payment_method = payment_method
+
+    def process(self, amount):
+        return self.payment_method.pay(amount)
+
+processor = PaymentProcessor(CardPayment())
+print(processor.process(100))
+
+processor = PaymentProcessor(CashPayment())
+print(processor.process(50))
+
+processor = PaymentProcessor(CryptoPayment())
+print(processor.process(200))
